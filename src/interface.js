6b020c7187cf6e724f5aaca946d90b46d152a60d
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-05 15:54:11 +0100

    up down event listeners working

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const thermostat = new Thermostat();
-  var temperature = document.querySelector(".temperature");
+  var temperature = document.querySelector(".temperature"),
+    temperatureUp = document.getElementById("temperature-up"),
+    temperatureDown = document.getElementById("temperature-down");
 
   temperature.innerHTML = thermostat._temp;
+
+  temperatureUp.addEventListener("click", () => {
+    thermostat.up();
+    temperature.innerHTML = thermostat._temp;
+  });
+
+  temperatureDown.addEventListener("click", () => {
+    thermostat.down();
+    temperature.innerHTML = thermostat._temp;
+  });
 });
