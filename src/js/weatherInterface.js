61f0c2df0941317bb10e6b0a779a80faf941b400
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-06 21:24:22 +0100

    Successing days of week implemented with JS

--- src/js/weatherInterface.js
+++ src/js/weatherInterface.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   var dateDayName = document.querySelector(".date-dayname"),
+    dayOne = document.getElementById("day-one"),
+    dayTwo = document.getElementById("day-two"),
+    dayThree = document.getElementById("day-three"),
+    dayFour = document.getElementById("day-four"),
+    currentDate = new Date(),
     days = [
       "Sunday",
       "Monday",
@@ -8,26 +13,41 @@ document.addEventListener("DOMContentLoaded", () => {
       "Thursday",
       "Friday",
       "Saturday",
-    ],
-    daysAbbreviated = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  (currentDate = new Date()),
-    (currentCity = document.getElementById("current-city"));
+    ];
 
+  var dayOneDate = new Date();
+  dayOneDate.setDate(new Date().getDate() + 1);
+
+  var dayTwoDate = new Date();
+  dayTwoDate.setDate(new Date().getDate() + 2);
+
+  var dayThreeDate = new Date();
+  dayThreeDate.setDate(new Date().getDate() + 3);
+
+  var dayFourDate = new Date();
+  dayFourDate.setDate(new Date().getDate() + 4);
+
+  currentCity = document.getElementById("current-city");
   dateDayName.innerHTML = days[currentDate.getDay()];
-  updateTemperature();
+  dayOne.innerHTML = days[dayOneDate.getDay()].substring(0, 3);
+  dayTwo.innerHTML = days[dayTwoDate.getDay()].substring(0, 3);
+  dayThree.innerHTML = days[dayThreeDate.getDay()].substring(0, 3);
+  dayFour.innerHTML = days[dayFourDate.getDay()].substring(0, 3);
 
-  function updateTemperature() {
-    currentCity.addEventListener("change", (event) => {
-      const city = event.target.value;
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`;
+  // updateTemperature();
 
-      fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          currentTemperature.innerHTML = data.main.temp;
-        });
-    });
-  }
+  // function updateTemperature() {
+  //   currentCity.addEventListener("change", (event) => {
+  //     const city = event.target.value;
+  //     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`;
+
+  //     fetch(url)
+  //       .then((response) => {
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         currentTemperature.innerHTML = data.main.temp;
+  //       });
+  //   });
+  // }
 });
