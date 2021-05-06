33d8a419494f8e8b72443eb0be8fe5155f793b28
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-06 22:29:27 +0100

    Updated JS to update weather conditions

--- src/js/weatherInterface.js
+++ src/js/weatherInterface.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   var dateDayName = document.querySelector(".date-dayname"),
+    weatherTemp = document.querySelector(".weather-temp"),
+    weatherPressure = document.getElementById("weather-pressure"),
+    weatherHumidity = document.getElementById("weather-humidity"),
+    weatherWind = document.getElementById("weather-speed"),
     dayOne = document.getElementById("day-one"),
     dayTwo = document.getElementById("day-two"),
     dayThree = document.getElementById("day-three"),
@@ -14,6 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
       "Friday",
       "Saturday",
     ];
+
+  updateWeather();
 
   var dayOneDate = new Date();
   dayOneDate.setDate(new Date().getDate() + 1);
@@ -34,20 +40,23 @@ document.addEventListener("DOMContentLoaded", () => {
   dayThree.innerHTML = days[dayThreeDate.getDay()].substring(0, 3);
   dayFour.innerHTML = days[dayFourDate.getDay()].substring(0, 3);
 
-  // updateTemperature();
+  // updates current weather conditions
 
-  // function updateTemperature() {
-  //   currentCity.addEventListener("change", (event) => {
-  //     const city = event.target.value;
-  //     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`;
+  function updateWeather() {
+    // currentCity.addEventListener("change", (event) => {
+    // const city = event.target.value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`;
 
-  //     fetch(url)
-  //       .then((response) => {
-  //         return response.json();
-  //       })
-  //       .then((data) => {
-  //         currentTemperature.innerHTML = data.main.temp;
-  //       });
-  //   });
-  // }
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        weatherTemp.innerHTML = data.main.temp + "°";
+        weatherPressure.innerHTML = data.main.pressure;
+        weatherHumidity.innerHTML = data.main.humidity + "%";
+        weatherWind.innerHTML = data.wind.speed + " km/h";
+      });
+    // });
+  }
 });
