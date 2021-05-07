0dc38ac6f125b5bc70ac0be4123cf15c272148d6
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-07 19:16:33 +0100

    Setting up Reset button

--- src/js/thermostatInterface.js
+++ src/js/thermostatInterface.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   var temperature = document.querySelector(".temperature"),
     innerTemperature = document.querySelector(".ext"),
-    temperatureUp = document.querySelector(".minus-btn"),
-    temperatureDown = document.querySelector(".plus-btn"),
+    temperatureUp = document.querySelector(".plus-btn"),
+    temperatureDown = document.querySelector(".minus-btn"),
+    temperatureColour1 = document.querySelector(".fill1"),
+    temperatureColour2 = document.querySelector(".fill2"),
     temperatureReset = document.getElementById("temperature-reset"),
     powerSavingMode = document.getElementById("psm"),
     powerSavingModeOn = document.getElementById("psm-on"),
@@ -42,5 +44,21 @@ document.addEventListener("DOMContentLoaded", () => {
   function updateTemp() {
     temperature.innerText = thermostat._temperature;
     innerTemperature.innerText = thermostat._temperature;
+    if (thermostat.usage() === "low-usage") {
+      temperatureColour1.style.background =
+        "-webkit-linear-gradient(top, #24ad1f 40%, #c0f011 100%)";
+      temperatureColour2.style.background =
+        "-webkit-linear-gradient(top, #24ad1f 40%, #c0f011 100%)";
+    } else if (thermostat.usage() === "medium-usage") {
+      temperatureColour1.style.background =
+        "-webkit-linear-gradient(top, #ff4900 20%, #ff4900 100%)";
+      temperatureColour2.style.background =
+        "-webkit-linear-gradient(top, #ff4900 20%, #ff4900 100%)";
+    } else {
+      temperatureColour1.style.background =
+        "-webkit-linear-gradient(top, #ff1100 20%, #ec1d0e 100%)";
+      temperatureColour2.style.background =
+        "-webkit-linear-gradient(top, #ff1100 20%, #ec1d0e 100%)";
+    }
   }
 });
